e23aa6c4051a66bd7e35c3735455a67dd25d1b98
Alee <alee14498@gmail.com>
2018-05-13 17:19:34 -0400

    Licensing stuff

--- include/string.h
+++ include/string.h
@@ -1,3 +1,22 @@
+/*************************************************************************
+*
+*   AleeOS String: This include file is for strings
+*   Copyright (C) 2018 AleeCorp
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*************************************************************************/
 #ifndef STRING_H
 #define STRING_H
 
@@ -9,7 +28,6 @@ uint16 strlength(string ch)
         return --i;
 }
 
-/* Added in episode 3*/ /*This function compares two strings and returns true (1) if they are equal or false (0) if they are not equal */
 
 uint8 strEql(string ch1,string ch2)                     
 {
